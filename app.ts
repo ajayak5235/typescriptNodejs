c15a796223fd4f13d0853abf3da5a68434929e65
ajayak5235 <131739918+ajayak5235@users.noreply.github.com>
2023-11-02 00:32:12 +0530

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,10 @@
+import { toASCII } from "punycode";
+
+import express from 'express'
+
+import todosRoutes from './routes/todos'
+const app = express();
+
+app.use(todosRoutes)
+
+app.listen({port:3000})
